5719212f8b44b5068711026c5856ae27b4f16694
odf <olaf.delgado@gmail.com>
2014-04-09 11:33:49 +1000

    make a trivial in-browser application

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 'use strict';
 
-var cc = require('ceci-core');
+var React = require('react');
+var cc    = require('ceci-core');
 
 var parse  = require('./parse-3dall');
 var search = require('./search');
+
+
+var $ = React.DOM;
+
+
+var Application = React.createClass({
+  render: function() {
+    return $.div(null, 'RCSR');
+  }
+});
+
+var app = Application();
+
+React.renderComponent(app, document.getElementById('react-main'));
