bf2e15b639db33ebafe6bad20216fb4c24b2061f
odf <olaf.delgado@gmail.com>
2014-04-09 11:58:17 +1000

    present an upload dialog on startup

--- src/app.js
+++ src/app.js
@@ -10,9 +10,64 @@ var search = require('./search');
 var $ = React.DOM;
 
 
-var Application = React.createClass({
+var loadFile = function(f) {
+  var result = cc.defer();
+  var reader = new FileReader();
+
+  reader.onload = function(e) {
+    result.resolve(e.target.result);
+  };
+
+  reader.readAsText(f);
+  return result;
+};
+
+
+var Uploader = React.createClass({
+  preventSubmit: function(event) {
+    event.preventDefault();
+  },
+  loadFile: function(event) {
+    var files = event.target.files;
+
+    if (files[0]) {
+      cc.go(function*() {
+        this.props.handleData(yield loadFile(files[0]));
+      }.bind(this)).then(null, function(ex) { throw ex; });
+    }
+  },
   render: function() {
-    return $.div(null, 'RCSR');
+    return $.div(null,
+                 $.h2(null, 'Locate 3Dall.txt'),
+                 $.form({ onSubmit: this.preventSubmit },
+                        $.fieldset(null,
+                                   $.legend(null, 'Select a file'),
+                                   $.input({ type: 'file',
+                                             onChange: this.loadFile }))));
+  }
+});
+
+
+var Application = React.createClass({
+  getInitialState: function() {
+    return {
+      data: null
+    }
+  },
+  handleUpload: function(data) {
+    this.setState({ data: parse(data) });
+  },
+  render: function() {
+    var page;
+
+    if (this.state.data)
+      page = $.div(null, "Read " + this.state.data.length + " structures");
+    else
+      page = Uploader({ handleData: this.handleUpload });
+
+    return $.div(null,
+                 $.h1(null, 'RCSR'),
+                 page);
   }
 });
 
