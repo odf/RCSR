27d35027e225d0193444586ee60bb4f941764115
odf <olaf.delgado@gmail.com>
2014-08-09 13:15:23 +1000

    enable browser caching in express server

--- src/server.js
+++ src/server.js
@@ -6,7 +6,19 @@ var home = process.argv[2];
 
 var app = express();
 
+var cacheTime = 365.25 * 24 * 60 * 60; // one year
+
+var expires = function() {
+  return new Date(Date.now() + cacheTime*1000).toUTCString();
+};
+
 app.use(express.compress());
+
+app.use(function(req, res, next) {
+  res.setHeader("Cache-Control", "public, max-age=" + cacheTime);
+  res.setHeader("Expires", expires());
+  next();
+});
 
 app.get('/public/*', function(req, res) {
   res.sendfile(req.params[0], { root: home });
