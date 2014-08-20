f860a33ae3102babdbbe7e23651c2d33fea6a604
odf <olaf.delgado@gmail.com>
2014-08-20 10:24:09 +1000

    change extension for files using ES6 generators to .es6.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   context: __dirname + "/src",
-  entry: [ "regenerator/runtime/dev", "./client.es6" ],
+  entry: [ "regenerator/runtime/dev", "./client.es6.js" ],
   output: {
     path: __dirname + "/public/js",
     filename: "app.js"
   },
   module: {
     loaders: [
-      { test: /\.es6$/, loader: "regenerator" }
+      { test: /\.es6\.js$/, loader: "regenerator" }
     ]
   }
 };
