5436d8b970f7bec0664904e8af107d44c18b44ea
odf <olaf.delgado@gmail.com>
2014-08-20 11:25:35 +1000

    simplify nodejs server and add request logging

--- src/server.js
+++ src/server.js
@@ -7,19 +7,8 @@ var home = process.argv[2];
 
 var app = express();
 
-var cacheTime = 365.25 * 24 * 60 * 60; // one year
-
-var expires = function() {
-  return new Date(Date.now() + cacheTime*1000).toUTCString();
-};
-
+app.use(express.logger('dev'));
 app.use(express.compress());
-
-app.use(function(req, res, next) {
-  res.setHeader("Cache-Control", "public, max-age=" + cacheTime);
-  res.setHeader("Expires", expires());
-  next();
-});
 
 app.get('/images/:type/:name.jpg', function(req, res) {
   var path = req.path;
@@ -29,32 +18,12 @@ app.get('/images/:type/:name.jpg', function(req, res) {
     res.sendfile('/images/placeholder.jpg', { root: home });
 });
 
-app.get('/:name.html', function(req, res) {
-  res.sendfile(req.path, { root: home });
-});
-
-app.get('/:name.css', function(req, res) {
-  res.sendfile(req.path, { root: home });
-});
-
-app.get('/images/*', function(req, res) {
-  res.sendfile(req.path, { root: home });
-});
-
-app.get('/js/*', function(req, res) {
-  res.sendfile(req.path, { root: home });
-});
-
-app.get('/help/*', function(req, res) {
-  res.sendfile(req.path, { root: home });
-});
-
-app.get('/data/*', function(req, res) {
-  res.sendfile(req.path, { root: home });
-});
-
 app.get('*', function(req, res) {
-  res.sendfile('app.html', { root: home });
+  var path = req.path;
+  if (fs.existsSync(home + path))
+    res.sendfile(path, { root: home });
+  else
+    res.sendfile('app.html', { root: home });
 });
 
 app.listen(3000);
