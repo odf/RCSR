0223495b64521293d4ddd10efeb6623146dd6aef
odf <olaf.delgado@gmail.com>
2014-09-10 20:37:14 +1000

    start an experiment in server-side rendering

---
+++ src/render.js
@@ -0,0 +1,22 @@
+'use strict';
+
+GLOBAL.localStorage = (function() {
+  var _store = {};
+
+  return {
+    setItem: function(key, value) { _store[key] = value; },
+    getItem: function(key) { return _store[key]; }
+  };
+})();
+
+
+var fs    = require('fs');
+var React = require('react');
+
+var Application = require('./app.es6');
+
+var path = process.argv[2];
+var html = React.renderComponentToString(Application({ path: path }));
+var layout = fs.readFileSync(process.argv[3], { encoding: 'utf8' });
+
+console.log(layout.replace(/Loading.../, html));
