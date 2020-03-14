a6c5676cea785ef5d98e48aba46a1ad15472c20f
odf <olaf.delgado@gmail.com>
2020-03-14 17:15:38 +1100

    check/ribbons: new file

---
+++ src/check/ribbons.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var checkStructure = function(structure, log) {
+  structure.warnings.forEach(function(s) { log(structure.symbol+' - '+s); });
+};
+
+
+var checkFile = function(data, log) {
+  var i, k, structure, nr, seen;
+
+  seen = {};
+
+  for (i = 0, k = 1; i < data.length; ++i, ++k) {
+    structure = data[i];
+    nr = structure.serialNumber;
+    if (nr != k) {
+      log(structure.symbol+' - serial number skipped from '+(k-1)+' to '+nr);
+      k = nr;
+    }
+    if (seen[structure.symbol])
+      log(structure.symbol+' - more than one structure with this symbol');
+    else
+      seen[structure.symbol] = true;
+
+    checkStructure(structure, log);
+  }
+};
+
+
+module.exports = checkFile;
